7c6aee61b83f6694ff9d0afcd36756c8b7e495b6
sankimchi <sankim17@mittymonarch.com>
2016-06-20 18:14:32 -0400

    checking in at the end of class

---
+++ hello world/hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by Kiwook Kim on 6/20/16.
+//  Copyright (c) 2016 sankim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
